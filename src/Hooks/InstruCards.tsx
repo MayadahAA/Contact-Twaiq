8cac4101d7fc805ec596a5198002d4b4abfe70c3
Mayadah <97852885+MayadahAA@users.noreply.github.com>
2023-09-11 00:20:28 +0300

    add status condition

--- src/Hooks/InstruCards.tsx
+++ src/Hooks/InstruCards.tsx
@@ -6,6 +6,7 @@ interface IUser {
   id: string;
   name: string;
   field: string;
+  status: string;
 }
 
 function Instrucards() {
@@ -29,52 +30,58 @@ function Instrucards() {
   return (
     <>
       {/* search input start*/}
-      <div>
+      <div className="flex justify-end p-5">
         <input
           type="text"
-          placeholder="search"
+          className="p-2 rounded-md bg-slate-50 border-2 border-slate-400"
+          placeholder="بحث"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
       </div>
 
-      <div>
+      <div className="flex flex-wrap justify-end w-full  h-full gap-4">
         {searchHandler.map((e) => (
           <div key={e.id}>
-            <div className="flex flex-wrap justify-start m-10 w-full h-full gap-4">
+            <div>
               {/* {instructor.map((e) => (
                 <div key={e.id}> */}
-                  <div className="bg-white items-center gap-5 flex flex-col p-10  text-center ">
-                    <div className="w-32 rounded-full">
-                      <img
-                        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-                        alt=""
-                        className="rounded-full"
-                      />
-                    </div>
-                    <div className="text-center">
-                      <h1> {e.name}</h1>
-                      <h2>{e.field}</h2>
-                    </div>
-                    <div className="flex flex-wrap w-3/4 gap-2 text-center items-center justify-center">
-                      <h3 className="bg-purple-50 rounded-full p-1 px-3 text-sm font-medium text-slate-800">
-                        #javascript
-                      </h3>
-                      <h3 className="bg-purple-50 rounded-full p-1 px-3 text-sm font-medium text-slate-800">
-                        #javascript
-                      </h3>
-                      <h3 className="bg-purple-50 rounded-full p-1 px-3 text-sm font-medium text-slate-800">
-                        #javascript
-                      </h3>
-                    </div>
-                    <Link to={`/request/${e.id}`} className="w-full">
-                      <button className="bg-purple-700 text-white p-2 w-full">
-                        Send Request
-                      </button>
-                    </Link>
+              <div className="bg-white items-center gap-5 flex flex-col p-10  text-center ">
+                <div
+                  className="w-32 rounded-full relative "
+                >
+                  <img
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+                    alt=""
+                    className="rounded-full relative"
+                  />
+                  <div className={`  rounded-full w-7 h-7 absolute right-1 bottom-2 border-white border-2 ${e.status === 'not availble' ? 'bg-slate-400' : 'bg-blue-600'}`}>
+                    
                   </div>
-                  {/*  card end */}
-                {/* </div> */}
+                </div>
+                <div className="text-center">
+                  <h1> {e.name}</h1>
+                  <h2>{e.field}</h2>
+                </div>
+                <div className="flex flex-wrap w-3/4 gap-2 text-center items-center justify-center">
+                  <h3 className="bg-purple-50 rounded-full p-1 px-3 text-sm font-medium text-slate-800">
+                    #javascript
+                  </h3>
+                  <h3 className="bg-purple-50 rounded-full p-1 px-3 text-sm font-medium text-slate-800">
+                    #javascript
+                  </h3>
+                  <h3 className="bg-purple-50 rounded-full p-1 px-3 text-sm font-medium text-slate-800">
+                    #javascript
+                  </h3>
+                </div>
+                <Link to={`/request/${e.id}`} className="w-full">
+                  <button className="bg-purple-700 text-white p-2 w-full">
+                    Send Request
+                  </button>
+                </Link>
+              </div>
+              {/*  card end */}
+              {/* </div> */}
               {/* // ))} */}
             </div>
           </div>
